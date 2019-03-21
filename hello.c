e6fd5ca87873bf717920d490f143583f67fe6f44
ramyakv7 <48794551+ramyakv7@users.noreply.github.com>
2019-03-21 17:10:52 +0400

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,12 @@
+         //Ramya K V
+        // Problem set 1
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+      //say hello
+{
+ string name=get_string ("What is your name?\n");
+    printf ("hello ,%s\n" ,name );
+}
+
